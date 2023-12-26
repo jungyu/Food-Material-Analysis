4660bc0aa8d7beb9ede7f4ab00347b7322a4d364
Aaron Yu <jungyuyu@gmail.com>
2023-12-27 05:59:08 +0800

    Update vision.py

--- api/vision.py
+++ api/vision.py
@@ -6,5 +6,24 @@ client = vision.ImageAnnotatorClient()
 class Vision:
     def __init__(self):
       self.status = "TEXT"
-    def detect_text(url_path):
-      print(url_path)
+    def detect_text(path):
+        client = vision.ImageAnnotatorClient()
+
+        # [START vision_python_migration_text_detection]
+        with open(path, "rb") as image_file:
+            content = image_file.read()
+
+        image = vision.Image(content=content)
+
+        response = client.text_detection(image=image)
+        texts = response.text_annotations
+        print("Texts:")
+
+        for text in texts:
+            return text.description
+
+        if response.error.message:
+            raise Exception(
+                "{}\nFor more info on error messages, check: "
+                "https://cloud.google.com/apis/design/errors".format(response.error.message)
+            )
